b82d9482cab811e8e700a0f1a9d3a24a4fb25318
0dp <admin@eudora.dk>
2016-02-20 19:16:25 +0100

    form edit

--- signup/form.php
+++ signup/form.php
@@ -5,10 +5,11 @@ function dc_registration_form_fields() {
  
 	ob_start(); ?>	
 		<h3 class="dc_header"><?php _e('Register New Account'); ?></h3>
+		<p>Only one person from your camp should sign up. You will be the contact!
  
 		<?php 
 		// show any error messages after form submission
-		dc_show_error_messages(); ?>
+		//dc_show_error_messages(); ?>
  
 		<form id="dc_registration_form" class="dc_form" action="" method="POST">
 			<fieldset>
@@ -22,11 +23,31 @@ function dc_registration_form_fields() {
 				</p>
 				<p>
 					<label for="dc_user_first"><?php _e('First Name'); ?></label>
-					<input name="dc_user_first" id="dc_user_first" type="text"/>
+					<input name="dc_user_first" id="dc_user_first" placeholder="First Name" type="text"/>
 				</p>
 				<p>
 					<label for="dc_user_last"><?php _e('Last Name'); ?></label>
-					<input name="dc_user_last" id="dc_user_last" type="text"/>
+					<input name="dc_user_last" id="dc_user_last" placeholder="Last Name" type="text"/>
+				</p>
+				<p>
+					<label for="dc_user_phone"><?php _e('Phone #'); ?></label>
+					<input name="dc_user_phone" id="dc_user_phone" type="tel"/>
+				</p>
+				<p>
+					<label for="dc_user_camp_name"><?php _e('Camp Name / Project Title'); ?></label>
+					<input name="dc_user_camp_name" id="dc_user_camp_name" placeholder="Camp Name / Project Title" type="text"/>
+				</p>
+				<p>
+					<label for="dc_user_participants"><?php _e('How many people will participate in your camp/project'); ?></label>
+					<input name="dc_user_participants" id="dc_user_participants" placeholder="Participants" type="text"/>
+				</p>
+				<p>
+					<label for="dc_user_project_desc"><?php _e('Project description - Include one sentence that describes your gift to Dream City 2016 (This will be used for press, website etc., in case you change your project to some degree, please send an updated description to dreamcity@roskilde-festival.dk)'); ?></label>
+					<textarea rows="3" name="dc_user_project_desc" id="dc_user_project_desc" placeholder="Project Description" type="text"></textarea>
+				</p>
+				<p>
+					<label for="dc_user_project_construction"><?php _e('Does your project include any construction work? (If yes, please send a description and drawing/picture to illustrate your concept to bygdc@roskilde-festival.dk)'); ?></label>
+					<textarea rows="3" name="dc_user_project_construction" id="dc_user_project_construction" class="form-control" placeholder="Contruction Plans" type="text"></textarea>
 				</p>
 				<p>
 					<label for="password"><?php _e('Password'); ?></label>
