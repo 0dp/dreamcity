7fd5025c1d4a73b131360cb300f2fe7cbf7cb566
bluebeardk <blue-bear@oncable.dk>
2016-03-08 18:08:06 +0100

    Added install calls
    Har tilføjet install class samt "dreamers" pending tager kun "dreamers
    nu.

--- install/db_install.php
+++ install/db_install.php
@@ -4,66 +4,75 @@ global $dc_db_version;
 $dc_db_version = '1.0';
 
 
-function dcdb_install () {
-   	global $wpdb;
-   	global $dc_db_version;
-
-   	$table_name = $wpdb->prefix . "dc_camp";
-
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "CREATE TABLE $table_name (
-  		camp_id mediumint(9) NOT NULL AUTO_INCREMENT,
-  		user_id int(10) NOT NULL,
-  		camp_name nvarchar(100) NOT NULL, 
-  		camp_description nvarchar(max) NOT NULL,
-  		camp_url varchar(70) DEFAULT '' NOT NULL,
-  		camp_imageURL varchar(55) DEFAULT '' NOT NULL,
-  		camp_iconURL varchar(55) DEFAULT '' NOT NULL,
-  		camp_residents smallint(9) DEFAULT 0 NOT NULL,
-		camp_notes nvarchar(max), DEFAULT NULL,
-  		UNIQUE KEY camp_id (camp_id)
-		) $charset_collate;";
-
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	dbDelta( $sql );
-
-	add_option( 'dc_db_version', $dc_db_version );
-
-}
-
-function dc_install_data() {
-	global $wpdb;
+Class dc_installer {
+    
 	
-	$welcome_name = 'Mr. WordPress';
-	$welcome_text = 'Congratulations, you just completed the installation!';
+	static function dc_db_install(){
+		dc_installer::install_db_camp();
+		dc_installer::install_db_camp_meta();		
+	}		
 	
-	$table_name = $wpdb->prefix . 'dc_camp';
-	
-	$wpdb->insert( 
-		$table_name, 
-		array( 
-			'user_id' => 0,
-			'camp_name' => $welcome_name, 
-			'camp_description' => $welcome_text,
-			'camp_url' => 'test',
-			'camp_imageURL' => '',
-			'camp_iconURL' => '',
-			'camp_residents' => '',
-			'camp_notes' => ''
-		) 
-	);
-}
+	static function install_db_camp(){
+        global $wpdb;
+		global $dc_db_version;
 
+		$table_name = $wpdb->prefix . "dc_camp";
+		$charset_collate = $wpdb->get_charset_collate();		
+		
+		$sql = "CREATE TABLE $table_name (
+			camp_id bigint(20) NOT NULL AUTO_INCREMENT,
+			user_id bigint(20) NOT NULL,
+			camp_name text(100) NOT NULL, 
+			camp_phone varchar(12) NOT NULL,
+			camp_description text NOT NULL,
+			camp_construction text NULL,
+			camp_url varchar(70) DEFAULT '' NULL,
+			camp_imageURL varchar(100) DEFAULT '' NULL,
+			camp_iconURL varchar(100) DEFAULT '' NULL,
+			camp_residents smallint(9) DEFAULT 0 NOT NULL,
+			camp_notes text DEFAULT NULL,
+			camp_registration_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			camp_modified datetime DEFAULT '0000-00-00 00:00:00' NULL,
+			UNIQUE KEY camp_id (camp_id)
+			) $charset_collate;";
 
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
 
-
-function dc_update_db_check() {
-    global $dc_db_version;
-    if ( get_site_option( 'dc_db_version' ) != $dc_db_version ) {
-        dc_install();
+		add_option( 'dc_db_version', $dc_db_version );
     }
+	
+	static function install_db_camp_meta(){
+        global $wpdb;
+		global $dc_db_version;
+
+		$table_name = $wpdb->prefix . "dc_camp_meta";
+		$charset_collate = $wpdb->get_charset_collate();		
+		
+		$sql = "CREATE TABLE $table_name (
+			meta_id bigint(20) NOT NULL AUTO_INCREMENT,
+			camp_id bigint(20) DEFAULT 0 NOT NULL,
+			meta_key varchar(255) DEFAULT NULL,
+			meta_value longtext DEFAULT NULL,		
+			UNIQUE KEY meta_id (meta_id)
+			) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+
+		add_option( 'dc_db_version', $dc_db_version );
+    }
+    
+	static function dc_update_db_check() {
+		global $dc_db_version;
+		if ( get_site_option( 'dc_db_version' ) != $dc_db_version ) {
+			dcdb_install();
+		}
+	}	
 }
 
+//register_activation_hook( plugin_dir_path( __FILE__ ) . 'install/db_install.php', 'dcdb_install' );
+
+//add_action( 'plugins_loaded', 'dc_update_db_check' );
 
 ?>
--- vendor/new-user-approve/admin/templates/approve.php
+++ vendor/new-user-approve/admin/templates/approve.php
@@ -13,17 +13,17 @@ if ( isset( $_GET['user'] ) && isset( $_GET['status'] ) ) {
 
 <div class="wrap">
 	<h2><?php _e( 'Dreamers Approval', 'new-user-approve' ); ?></h2>
-
+	
 	<?php wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false ); ?>
 	<?php wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', false ); ?>
 
 	<div id="poststuff" class="columns metabox-holder">
 		<div id="post-body" class="metabox-holder columns-2">
 			<div id="postbox-container-1" class="postbox-container column-secondary">
-				<?php //do_meta_boxes( 'users_page_new-user-approve-admin', 'side', $this ); ?>
+				<?php //do_meta_boxes( 'users_page_new-user-approve-admin', 'side', $this ); ?>				
 			</div>
-			<div id="postbox-container-2" class="postbox-container column-primary">
-				<?php do_meta_boxes( 'users_page_new-user-approve-admin', 'main', '' ); ?>
+			<div id="postbox-container-2" class="postbox-container column-primary">				
+				<?php do_meta_boxes( 'users_page_new-user-approve-admin', 'main', '' ); ?>				
 			</div>
 		</div>
 	</div>
