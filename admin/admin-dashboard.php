ddfeed110d405e74d2a857290f5b1837359148cc
0dp <admin@eudora.dk>
2016-03-13 21:12:44 +0100

    shitty backend

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -2,22 +2,51 @@
 /* Admin Dashboard */
 ?>
 <div class="wrap">
-    <h2><?php _e( 'Approved Dream-city Projects', 'new-user-approve' ); ?></h2>
+    <h2><?php _e( 'Dream-city Projects', 'new-user-approve' ); ?></h2>
 
 
 <?php 
-$result = $wpdb->get_results( "SELECT wp0_users.display_name, wp0_users.user_email,  wp0_dc_camp.*,wp0_dc_camp_meta.*
+$approved = $wpdb->get_results( 
+"SELECT wp0_users.display_name, wp0_users.user_email, wp0_usermeta.*, wp0_dc_camp.*,wp0_dc_camp_meta.*
 FROM wp0_dc_camp 
 LEFT JOIN wp0_dc_camp_meta ON wp0_dc_camp.camp_id = wp0_dc_camp_meta.camp_id
 LEFT JOIN wp0_users ON wp0_dc_camp.user_id = wp0_users.ID
+LEFT JOIN wp0_usermeta ON wp0_usermeta.user_id = wp0_users.ID WHERE wp0_usermeta.meta_value = 'approved'
 "); 
 
-// echo "<pre>"; print_r($result); echo "</pre>";
+$pending = $wpdb->get_results( 
+"SELECT wp0_users.display_name, wp0_users.user_email, wp0_usermeta.*, wp0_dc_camp.*,wp0_dc_camp_meta.*
+FROM wp0_dc_camp 
+LEFT JOIN wp0_dc_camp_meta ON wp0_dc_camp.camp_id = wp0_dc_camp_meta.camp_id
+LEFT JOIN wp0_users ON wp0_dc_camp.user_id = wp0_users.ID
+LEFT JOIN wp0_usermeta ON wp0_usermeta.user_id = wp0_users.ID WHERE wp0_usermeta.meta_value = 'pending'
+"); 
+
+$denied = $wpdb->get_results( 
+"SELECT wp0_users.display_name, wp0_users.user_email, wp0_usermeta.*, wp0_dc_camp.*,wp0_dc_camp_meta.*
+FROM wp0_dc_camp 
+LEFT JOIN wp0_dc_camp_meta ON wp0_dc_camp.camp_id = wp0_dc_camp_meta.camp_id
+LEFT JOIN wp0_users ON wp0_dc_camp.user_id = wp0_users.ID
+LEFT JOIN wp0_usermeta ON wp0_usermeta.user_id = wp0_users.ID WHERE wp0_usermeta.meta_value = 'denied'
+"); 
+
+ //echo "<pre>"; print_r($result); echo "</pre>";
 
 
 
 ?>
-<table class="table table-striped table-bordered">
+
+
+<ul class="nav nav-tabs">
+  <li role="presentation" class="active"><a data-toggle="tab" href="#pending">Pending Projects</a></li>
+  <li role="presentation"><a data-toggle="tab" href="#approved">Approved</a></li>
+  <li role="presentation"><a data-toggle="tab" href="#denied">Denied</a></li>
+</ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane fade in active" id="pending">
+        <table class="table table-striped table-bordered">
         <thead>
             <tr>
                 <th>Camp Name</th>
@@ -32,7 +61,7 @@ LEFT JOIN wp0_users ON wp0_dc_camp.user_id = wp0_users.ID
             </tr>
         </thead>
         <tbody>
-            <?php foreach($result as $row) { ?>
+            <?php foreach($pending as $row) { ?>
             <tr>
                 <td><?php echo $row->camp_name; ?></td>
                 <td><?php echo $row->display_name; ?></td>
@@ -46,6 +75,71 @@ LEFT JOIN wp0_users ON wp0_dc_camp.user_id = wp0_users.ID
             <?php } ?>
         </tbody>
     </table>
-</div>
+    </div>
+    <div role="tabpanel" class="tab-pane fade" id="approved">
+                <table class="table table-striped table-bordered">
+        <thead>
+            <tr>
+                <th>Camp Name</th>
+                <th>Contact Person</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>#Residents</th>
+                <th>Project Description</th>
+                <th>Project Construction</th>
+                <th>Workshop</th>
 
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($approved as $row) { ?>
+            <tr>
+                <td><?php echo $row->camp_name; ?></td>
+                <td><?php echo $row->display_name; ?></td>
+                <td><?php echo $row->user_email; ?></td>
+                <td><?php echo $row->camp_phone; ?></td>
+                <td><?php echo $row->camp_residents; ?></td>
+                <td><?php echo $row->camp_description; ?></td>
+                <td><?php echo $row->camp_construction; ?></td>
+                <td><?php echo $row->meta_value; ?></td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+    </div>
+    <div role="tabpanel" class="tab-pane fade" id="denied">
+                <table class="table table-striped table-bordered">
+        <thead>
+            <tr>
+                <th>Camp Name</th>
+                <th>Contact Person</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>#Residents</th>
+                <th>Project Description</th>
+                <th>Project Construction</th>
+                <th>Workshop</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($denied as $row) { ?>
+            <tr>
+                <td><?php echo $row->camp_name; ?></td>
+                <td><?php echo $row->display_name; ?></td>
+                <td><?php echo $row->user_email; ?></td>
+                <td><?php echo $row->camp_phone; ?></td>
+                <td><?php echo $row->camp_residents; ?></td>
+                <td><?php echo $row->camp_description; ?></td>
+                <td><?php echo $row->camp_construction; ?></td>
+                <td><?php echo $row->meta_value; ?></td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+    </div>
+  </div>
+
+
+</div>
 
