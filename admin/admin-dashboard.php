3523aa31f33a529a0694bc8ca564457e7c3c1beb
0dp <admin@eudora.dk>
2016-03-24 22:18:41 +0100

    admin db list fix

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -7,27 +7,27 @@
 
 <?php 
 $approved = $wpdb->get_results( 
-"SELECT wp0_users.display_name, wp0_users.user_email, wp0_usermeta.*, wp0_dc_camp.*,wp0_dc_camp_meta.*
+"SELECT DISTINCT wp0_users.display_name, wp0_users.user_email, wp0_usermeta.*, wp0_dc_camp.*,wp0_dc_camp_meta.*
 FROM wp0_dc_camp 
 LEFT JOIN wp0_dc_camp_meta ON wp0_dc_camp.camp_id = wp0_dc_camp_meta.camp_id
 LEFT JOIN wp0_users ON wp0_dc_camp.user_id = wp0_users.ID
-LEFT JOIN wp0_usermeta ON wp0_usermeta.user_id = wp0_users.ID WHERE wp0_usermeta.meta_value = 'approved'
+LEFT JOIN wp0_usermeta ON wp0_usermeta.user_id = wp0_users.ID WHERE wp0_usermeta.meta_value = 'approved' GROUP BY wp0_users.ID
 "); 
 
 $pending = $wpdb->get_results( 
-"SELECT wp0_users.display_name, wp0_users.user_email, wp0_usermeta.*, wp0_dc_camp.*,wp0_dc_camp_meta.*
+"SELECT DISTINCT wp0_users.display_name, wp0_users.user_email, wp0_usermeta.*, wp0_dc_camp.*,wp0_dc_camp_meta.*
 FROM wp0_dc_camp 
 LEFT JOIN wp0_dc_camp_meta ON wp0_dc_camp.camp_id = wp0_dc_camp_meta.camp_id
 LEFT JOIN wp0_users ON wp0_dc_camp.user_id = wp0_users.ID
-LEFT JOIN wp0_usermeta ON wp0_usermeta.user_id = wp0_users.ID WHERE wp0_usermeta.meta_value = 'pending'
+LEFT JOIN wp0_usermeta ON wp0_usermeta.user_id = wp0_users.ID WHERE wp0_usermeta.meta_value = 'pending' GROUP BY wp0_users.ID
 "); 
 
 $denied = $wpdb->get_results( 
-"SELECT wp0_users.display_name, wp0_users.user_email, wp0_usermeta.*, wp0_dc_camp.*,wp0_dc_camp_meta.*
+"SELECT DISTINCT wp0_users.display_name, wp0_users.user_email, wp0_usermeta.*, wp0_dc_camp.*,wp0_dc_camp_meta.*
 FROM wp0_dc_camp 
 LEFT JOIN wp0_dc_camp_meta ON wp0_dc_camp.camp_id = wp0_dc_camp_meta.camp_id
 LEFT JOIN wp0_users ON wp0_dc_camp.user_id = wp0_users.ID
-LEFT JOIN wp0_usermeta ON wp0_usermeta.user_id = wp0_users.ID WHERE wp0_usermeta.meta_value = 'denied'
+LEFT JOIN wp0_usermeta ON wp0_usermeta.user_id = wp0_users.ID WHERE wp0_usermeta.meta_value = 'denied' GROUP BY wp0_users.ID
 "); 
 
  //echo "<pre>"; print_r($result); echo "</pre>";
